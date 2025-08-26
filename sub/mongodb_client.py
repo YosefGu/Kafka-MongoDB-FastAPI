2e3f3fa2ffa683c2a94529643e54468c6dea1710
YosefGu <yos770yos@gmail.com>
2025-08-26 17:52:53 +0300

    updadte mongo client to connect on container

--- sub/mongodb_client.py
+++ sub/mongodb_client.py
@@ -5,7 +5,12 @@ from datetime import datetime
 class MongodbClient():
 
     def __init__(self):
-        self.client = MongoClient(os.getenv('MONGO_CONN'))
+        self.user = os.getenv('MONGO_USER')
+        self.password = os.getenv('MONGO_PASSWORD')
+        self.host = os.getenv('MONGO_HOST')
+        self.port = os.getenv('MONGO_PORT', '27017')
+
+        self.client = MongoClient(f'mongodb://{self.user}:{self.password}@{self.host}:{self.port}/')
         self.database = self.client['kafka-project']
 
     def save_data(self, collection, data):
